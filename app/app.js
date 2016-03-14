c701a5597e1a4c81134419c3679016386e1d5ac9
andela-bmosigisi <brian.mosigisi@andela.com>
2016-03-14 17:09:59 +0300

    (feature) use angular for auth

--- app/app.js
+++ app/app.js
@@ -1,3 +1,20 @@
 'use strict';
 
-angular.module('myApp', []);
+require('./js/auth.js');
+
+angular.module('spreeder', ['spreeder.auth', 'ngRoute'])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+      when('/login', {
+        templateUrl: '/views/login.html',
+        controller: 'LoginCtrl'
+      }).
+      when('/signup', {
+        templateUrl: '/views/signup.html',
+        controller: 'SignupCtrl'
+      }).
+      otherwise({
+        redirectTo: '/login'
+      });
+    }
+  ]);
