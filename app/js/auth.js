9430017e2bc2352a2f40bf9bf8007d0e43c8f9e4
andela-bmosigisi <brian.mosigisi@andela.com>
2016-03-15 20:54:39 +0300

    (feature) fix login styling

--- app/js/auth.js
+++ app/js/auth.js
@@ -1,6 +1,6 @@
 angular.module('spreeder.auth', [])
   .controller('AuthCtrl', ['$scope', '$location', function($scope, $location) {
-    if ($location.path() == 'login') {
+    if ($location.path() == '/login') {
       $scope.login_active = true;
     } else {
       $scope.login_active = false;
