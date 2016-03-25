71a1cc21fc44f12585961022ae7d613ca33b4d05
andela-bmosigisi <brian.mosigisi@andela.com>
2016-03-25 15:13:33 +0300

    (feature) submit speed reading text

--- app/js/dash.js
+++ app/js/dash.js
@@ -8,15 +8,40 @@ angular.module('spreeder.dash', ['ngRoute'])
         templateUrl: '/views/get_text.html',
         controller: 'DashCtrl'
       }).
+      when('/dashboard/spreed/start', {
+        templateUrl: '/views/start_spreed.html',
+        controller: 'SpreedCtrl'
+      }).
       when('/dashboard/metrics', {
         templateUrl: '/views/metrics.html',
         controller: 'DashCtrl'
       })
   }])
-  .controller('DashCtrl', ['$scope',
-    function($scope) {
+  .controller('DashCtrl', ['$scope', '$location', 'spreedService',
+    function($scope, $location, spreedService) {
       $scope.speedRead = function() {
-
+        if ($scope.getText.$valid) {
+          spreedService.speed = $scope.speed;
+          spreedService.chunkSize = $scope.chunkSize;
+          spreedService.text = $scope.pasteText;
+          $location.path('/dashboard/spreed/start');
+        } else {
+          alert('Minimum words: 100');
+        }
       };
     }
-  ]);
+  ])
+  .controller('SpreedCtrl', ['$scope', 'spreedService',
+    function($scope, spreedService) {
+      $scope.text = spreedService.text;
+    }
+  ])
+  .factory('spreedService', function() {
+    var spreedData = {};
+
+    spreedData.chunkSize = 1;
+    spreedData.speed = 300;
+    spreedData.text = '';
+
+    return spreedData;
+  });
